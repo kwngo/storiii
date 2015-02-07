c878ea9d40ef49defd3b1044931a5c1108aa6c3c
kwngo <kareem.kwong@gmail.com>
2015-02-07 11:26:49 +0800

    Added stories controller

--- www/js/app.js
+++ www/js/app.js
@@ -5,15 +5,28 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
+.controller('StoryCtrl'function($scope) {
+    $scope.stories = [{
+        title: 'Snow White',
+        description: '',
+        reviews: [0, 1]
+
+    }, {
+        title: 'Cinderalla',
+        description: '',
+        reviews: [0, 1]
+    }]
+});
+
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
