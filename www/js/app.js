87388b15a59a8a552bdd5ef4d23cc47734c90612
kwngo <kareem.kwong@gmail.com>
2015-02-07 13:32:32 +0800

    Scrapped all the old code

--- www/js/app.js
+++ www/js/app.js
@@ -3,11 +3,11 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-'use strict'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-var app = angular.module('starter', ['ionic']);
-
-app.run(function($ionicPlatform) {
+.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
@@ -15,23 +15,88 @@ app.run(function($ionicPlatform) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
         if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
             StatusBar.styleDefault();
         }
     });
 })
 
+.config(function($stateProvider, $urlRouterProvider) {
 
-var controllers = angular.module('controllers', []);
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-controllers.controller('StoryCtrl', function($scope) {
-    $scope.stories = [{
-        title: 'Snow White',
-        description: '',
-        reviews: [0, 1]
+    // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+    })
 
-    }, {
-        title: 'Cinderalla',
-        description: '',
-        reviews: [0, 1]
-    }]
+    // Each tab has its own nav history stack:
+
+    .state('tab.dash', {
+        url: '/dash',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+            }
+        }
+    })
+
+    .state('tab.chats', {
+            url: '/chats',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/tab-chats.html',
+                    controller: 'ChatsCtrl'
+                }
+            }
+        })
+        .state('tab.chat-detail', {
+            url: '/chats/:chatId',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/chat-detail.html',
+                    controller: 'ChatDetailCtrl'
+                }
+            }
+        })
+
+    .state('tab.friends', {
+            url: '/friends',
+            views: {
+                'tab-friends': {
+                    templateUrl: 'templates/tab-friends.html',
+                    controller: 'FriendsCtrl'
+                }
+            }
+        })
+        .state('tab.friend-detail', {
+            url: '/friend/:friendId',
+            views: {
+                'tab-friends': {
+                    templateUrl: 'templates/friend-detail.html',
+                    controller: 'FriendDetailCtrl'
+                }
+            }
+        })
+
+    .state('tab.account', {
+        url: '/account',
+        views: {
+            'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+            }
+        }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
 });
