fde4e2b0321fbf82aa6e4d8c5c59eb383e7f97ba
kwngo <kareem.kwong@gmail.com>
2015-02-07 11:34:39 +0800

    Fixed mistakes with variable naming

--- www/js/app.js
+++ www/js/app.js
@@ -3,22 +3,11 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+'use strict';
 
-.controller('StoryCtrl'function($scope) {
-    $scope.stories = [{
-        title: 'Snow White',
-        description: '',
-        reviews: [0, 1]
+var app = angular.module('starter', ['ionic']);
 
-    }, {
-        title: 'Cinderalla',
-        description: '',
-        reviews: [0, 1]
-    }]
-});
-
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
@@ -30,3 +19,19 @@ angular.module('starter', ['ionic'])
         }
     });
 })
+
+
+var controllers = angular.module('controllers', []);
+
+controllers.controller('StoryCtrl', function($scope) {
+    $scope.stories = [{
+        title: 'Snow White',
+        description: '',
+        reviews: [0, 1]
+
+    }, {
+        title: 'Cinderalla',
+        description: '',
+        reviews: [0, 1]
+    }]
+});
